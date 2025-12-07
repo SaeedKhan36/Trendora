70d5da2e5f0257103fafc00c876a4632e8269716
SaeedKhan36 <saeedkhan364.com@gmail.com>
2025-12-07 11:38:55 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Cart from './pages/Cart'
 import Home from './pages/home'
 import Collection from './pages/collection'
@@ -7,12 +7,18 @@ import Contact from './pages/contact'
 import Product from './pages/Product'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Login from './pages/login'
+import About from './pages/about'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
-    <div className=''>
-      <Router>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+
+
+      <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Cart' element={< Cart />} />
         <Route path='/Collection' element={<Collection />} />
@@ -20,10 +26,12 @@ const App = () => {
         <Route path='/Product:productId' element={<Product />} />
         <Route path='/PlaceOrder' element={<PlaceOrder />} />
         <Route path='/Orders' element={<Orders />} />
-        <Route path='/login' element={</>}
+        <Route path='/login' element={<Login />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/about' element={< About />} />
 
 
-    </Router>
+      </Routes>
 
     </div >
   )
